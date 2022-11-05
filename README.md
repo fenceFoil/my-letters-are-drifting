41d1ed97e1ecc193af8aab5c9ce85566c6bed17a
fenceFoil <karnavasbj@gmail.com>
2022-11-04 23:02:56 -0400

    Initial commit for real now

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # my-letters-are-drifting
+
+![](doc/whiteboard1.png)
+
+Put the banana keys in `.env`, which should look like this:
+
+```ini
+api_key=2222b2d2-22a2-2222-b222-2e222a222d22
+model_key=4444b4d4-44a4-4444-b444-4e444a444d44
+```
---
+++ a.py
@@ -0,0 +1,28 @@
+import banana_dev as banana
+import base64
+from io import BytesIO
+from PIL import Image
+from configparser import ConfigParser
+
+model_inputs = {
+	"prompt": "piles of brain drugs",
+	"num_inference_steps":50,
+	"guidance_scale":9,
+	"height":512,
+	"width":512,
+	"seed":666
+}
+
+config = ConfigParser()
+config.read('vars.env')
+
+# Run the model
+out = banana.run(config['DEFAULT']['api_key'] , config['DEFAULT']['model_key'], model_inputs)
+
+# Extract the image and save to output.jpg
+image_byte_string = out["modelOutputs"][0]["image_base64"]
+image_encoded = image_byte_string.encode('utf-8')
+image_bytes = BytesIO(base64.b64decode(image_encoded))
+image = Image.open(image_bytes)
+image.save("output.jpg")
+
